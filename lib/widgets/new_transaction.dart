b9791287531a1edc1556431a0651551a090321e3
quangkhatran <quangkhatran@gmail.com>
2022-06-11 09:50:09 +0700

    allow input a new transaction and show the list of transactions dynamically

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final Function addTx;
+  String titleInput;
+  String amountInput;
+
+  NewTransaction(this.addTx);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                onChanged: (val) {
+                  titleInput = val;
+                },
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                onChanged: (val) {
+                  amountInput = val;
+                },
+              ),
+              FlatButton(
+                child: Text('Add Transaction'),
+                textColor: Colors.purple,
+                onPressed: () {
+                  print(titleInput);
+                  print(amountInput);
+                  addTx(titleInput,double.parse(amountInput));
+                },
+              )
+            ]),
+      ),
+    );
+  }
+}
