2c7146840ceb0e0ceb7d4517a67868482f57afb9
quangkhatran <quangkhatran@gmail.com>
2022-06-12 07:30:10 +0700

    add TextTheme

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(headline6:TextStyle(fontFamily: 'OpenSans', fontSize: 18, fontWeight: FontWeight.bold,)),
+        appBarTheme: AppBarTheme(textTheme: ThemeData.light().textTheme.copyWith(headline6:TextStyle(fontFamily: 'OpenSans', fontSize: 20, fontWeight: FontWeight.bold,)),),
       ),
       home: MyHomePage(),
     );
@@ -65,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Personal Expenses'),
+        title: Text('Personal Expenses', style: TextStyle(fontFamily: 'OpenSans'),),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
